c6e0fb1f7fe10b3a4d6110beac84d8bf4d0e3e32
lge2002 <leapge2025@gmail.com>
2025-06-13 14:22:54 +0530

    Add Windy plugin files

---
+++ plugin.js
@@ -0,0 +1,27 @@
+windyInit({}, windyAPI => {
+  const { map } = windyAPI;
+
+  const geoJsonData = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": { "name": "Test Polygon" },
+        "geometry": {
+          "type": "Polygon",
+          "coordinates": [[[90, 20], [95, 20], [95, 25], [90, 25], [90, 20]]]
+        }
+      }
+    ]
+  };
+
+  const geoJsonLayer = L.geoJSON(geoJsonData, {
+    style: {
+      color: "#FF0000",
+      weight: 2,
+      opacity: 0.65
+    }
+  }).addTo(map);
+
+  console.log("GeoJSON overlay loaded on Windy.");
+});
